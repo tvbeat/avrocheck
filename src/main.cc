077f37df5266597bd24f9ab33a67068790f68ae4
alan pavičić <aka@svrg.net>
2017-10-17 16:24:36 +0200

    bss epg schema supported

--- src/main.cc
+++ src/main.cc
@@ -10,7 +10,7 @@
 
 
 static void usage() {
-  puts("usage: avrocheck plus_events|q_events\n"
+  puts("usage: avrocheck plus_events|q_events|epg_sched\n"
        "avro file should be provided from standard input\n"
        "AE_SHARE should point to directory with avsc files (schema definitions)");
 }
@@ -19,18 +19,17 @@ template <typename T>
 void read_from_input_stream() {
   try {
     avro_reader<T> reader;
-    puts("schema ok");
+    puts("schema loaded");
 
     int _;
     reader.reduce( _
                  , [](const typename T::type &, int&) { stats::count("records read"); }
                  , [](int&)                           { stats::count("read errors");  }
                  );
-    stats::dump();
-    } catch (...) {
-      puts("can't instantiate or wrong schema");
-      exit(-1);
-   }
+  } catch (avro::Exception &e) {
+    printf("error: %s\n", e.what());
+  }
+  stats::dump();
 }
 
 int main(int argc, char **argv) {
@@ -42,6 +41,7 @@ int main(int argc, char **argv) {
   std::string type = argv[1];
   if      (type == "plus_events") { read_from_input_stream<avro_events_plus>(); }
   else if (type == "q_events"   ) { read_from_input_stream<avro_events_q>();    }
+  else if (type == "epg_sched"  ) { read_from_input_stream<bss_epg_sched>();    }
   else                            { usage(); return -1; }
   return 0;
 
